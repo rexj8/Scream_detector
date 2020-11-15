546fc812bb89e35f9665327ba27abfb333b13c06
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-11-15 18:27:51 +0530

    added files

---
+++ test_ffmpeg.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+from vosk import Model, KaldiRecognizer, SetLogLevel
+import sys
+import os
+import wave
+import subprocess
+
+SetLogLevel(0)
+
+sample_rate=16000
+model = Model("model")
+rec = KaldiRecognizer(model, sample_rate)
+
+process = subprocess.Popen(['ffmpeg', '-loglevel', 'quiet', '-i',
+                            sys.argv[1],
+                            '-ar', str(sample_rate) , '-ac', '1', '-f', 's16le', '-'],
+                            stdout=subprocess.PIPE)
+
+file=open("speech_text.txt", "w+")
+
+while True:
+    data = process.stdout.read(4000)
+    if len(data) == 0:
+        break
+    if rec.AcceptWaveform(data):
+        pass
+        print(rec.Result())
+        file.write(rec.Result() + " ")
+    # else:
+        print(rec.PartialResult())
+        file.write(rec.PartialResult()+" ")
+
+print(rec.FinalResult())
+file.write(rec.FinalResult() + " ")
+
