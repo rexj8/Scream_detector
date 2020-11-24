ba22a3288a960132b2343ffae9e75c243db49972
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-11-24 23:45:01 +0530

    finally text from audio extracted

--- Negative_word_detector.py
+++ Negative_word_detector.py
@@ -13,7 +13,6 @@ voice_text_file = open('final_speech_to_text.txt','r')
 file_string = [(line.strip()).split() for line in voice_text_file]
 
 i=len(file_string[0])-1
-print(i)
 while i>=0:
     sub = file_string[0][i]
     index = file[file['words']==sub].index.values
