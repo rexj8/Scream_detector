c9c919809174b08c874dafcd1712855e444ab7f5
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-11-24 23:31:46 +0530

    finally text from audio extracted

--- Negative_word_detector.py
+++ Negative_word_detector.py
@@ -3,7 +3,7 @@ import pandas as pd
 file = pd.read_csv('abusive_words.csv')
 critical_index = 0
 
-file.dropna(inplace = True)
+# file.dropna(inplace = True)
 
 
 
@@ -14,11 +14,16 @@ file_string = [(line.strip()).split() for line in voice_text_file]
 
 i=len(file_string[0])-1
 while i>=0:
+# sub = 'help'
     sub = file_string[0][i]
-    critical_index = file["words"].str.find(sub)
+    # critical_index = file["words"].str.find(sub)
+
+    index = file[file['words']==sub].index.values
+
+    critical_index+=file._get_value(index,'N_index')
 
     # file["Indexes"]= file["words"].str.find(sub)
 
 
 
-print()
+print(file[file['words']==sub].index.values)
