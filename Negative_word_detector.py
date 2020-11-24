6dd0a73739885dd1deb21fbaf6a7d7b49080c7f5
rexj8 <rachit.gla_cs18@gla.ac.in>
2020-11-24 23:46:18 +0530

    finally getting critical index done

--- Negative_word_detector.py
+++ Negative_word_detector.py
@@ -3,11 +3,6 @@ import pandas as pd
 file = pd.read_csv('abusive_words.csv')
 critical_index = 0
 
-# file.dropna(inplace = True)
-
-
-
-
 voice_text_file = open('final_speech_to_text.txt','r')
 
 file_string = [(line.strip()).split() for line in voice_text_file]
